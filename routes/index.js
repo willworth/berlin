70cf50c14baefa0c6c61f7ad5431c001f3742bfd
Will Worth <will.worth@gmail.com>
2018-04-13 18:50:49 +0200

    pug demo

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
+
 const router = express.Router();
 
 // Do work here
 router.get('/about', (req, res) => {
-    // res.send('Hey! It only bloody works!');
-    res.render('about');
+  // res.send('Hey! It only bloody works!');
+  res.render('about', { examplePerson: 'Brando', list: ['Malaga', 'Rio'] });
 });
 router.get('/', (req, res) => {
-    // res.send('Hey! It only bloody works!');
-    res.render('hello');
+  // res.send('Hey! It only bloody works!');
+  res.render('hello');
 });
 
 module.exports = router;
